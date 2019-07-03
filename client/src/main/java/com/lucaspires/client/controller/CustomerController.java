60f80342860e9408b804037e4bb62dc40b64ccb0
Lucas Pires <lucas_pires@outlook.com>
2019-07-03 15:39:11 -0300

    Creating a controller

---
+++ client/src/main/java/com/lucaspires/client/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.lucaspires.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
