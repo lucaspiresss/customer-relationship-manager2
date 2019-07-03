4cab797a5a44865b1cb29d1c14a327aeda3b9c46
Lucas Pires <lucas_pires@outlook.com>
2019-07-03 15:36:10 -0300

    Adding RestTemplate on application

--- client/src/main/java/com/lucaspires/client/ClientApplication.java
+++ client/src/main/java/com/lucaspires/client/ClientApplication.java
@@ -2,11 +2,19 @@ package com.lucaspires.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+	
+	// Bean for RestTemplate (used to make client REST calls)
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
