b86ea40564a1f2382ddb9636cb797af540b3b72d
Lucas Pires <lucas_pires@outlook.com>
2019-07-03 10:58:13 -0300

    Adding some comments on the code

--- api/src/main/java/com/lucaspires/api/rest/CustomerRestController.java
+++ api/src/main/java/com/lucaspires/api/rest/CustomerRestController.java
@@ -33,7 +33,7 @@ public class CustomerRestController {
 		return customerService.getCustomers();
 	}
 	
-	// Add mapping for POST /customers - add a new customer
+	// Add mapping for POST /customers - create a new customer
 	@PostMapping("/customers")
 	public String addCustomer(@RequestBody Customer theCustomer) {
 		// If an id is passed in the JSON, set the id to 0 to force to save a new customer, instead of update
@@ -46,7 +46,7 @@ public class CustomerRestController {
 		return "Customer added successfully";
 	}
 	
-	// Add mapping for DELETE /customers/{customerId} - delete a customer
+	// Add mapping for DELETE /customers/{customerId} - delete an existing customer
 	@DeleteMapping("/customers/{customerId}")
 	public String deleteCustomer(@PathVariable int customerId) {
 		// Call a method to get the customer who will be deleted
@@ -64,7 +64,7 @@ public class CustomerRestController {
 		return "Customer " + customerId + " deleted successfully";
 	}
 	
-	// Add mapping for GET /customers/{customerId} - return a customer
+	// Add mapping for GET /customers/{customerId} - return a single customer
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		// Call a method to get the customer
