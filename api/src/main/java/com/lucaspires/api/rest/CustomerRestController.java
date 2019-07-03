455090e8234c62f93b72c408bdf870769799d68e
Lucas Pires <lucas_pires@outlook.com>
2019-07-02 21:19:11 -0300

    Creating REST controller

---
+++ api/src/main/java/com/lucaspires/api/rest/CustomerRestController.java
@@ -0,0 +1,10 @@
+package com.lucaspires.api.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+}
